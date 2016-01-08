e87a19d65d4327ab87a58857369458bd720ce7ea
Jeremy Stafford <draepetan@live.com>
2016-01-07 23:17:47 -0700

    Initial

---
+++ NotifyIcon_MDDemo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using NotifyIcon_MDDemo.Annotations;
+
+namespace NotifyIcon_MDDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _items;
+
+        public ObservableCollection<string> Items
+        {
+            get { return _items; }
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var testItems = new List<string>
+            {
+                "Sub Item 1",
+                "Sub Item 2",
+                "Sub Item 3"
+            };
+
+            Items = new ObservableCollection<string>(testItems);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
